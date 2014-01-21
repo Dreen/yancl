308cbdd6c9192047bc9324f8f6456fd7d7c362a8
Greg Balaga <greg.balaga@gmail.com>
2014-01-21 11:19:54 +0000

    working with static functions

--- yancl.js
+++ yancl.js
@@ -15,20 +15,35 @@ function jquerify(html)
 	};
 }
 
-function YANCL ()
+/**
+headers = {};
+body = null; // need underscore method for checking
+url = "";
+method = "GET";
+*/
+function Yancl (extension)
 {
-
+	for (k in extension)
+	{
+		console.log('ext[%j] = %j', k, extension[k]);
+		this[k] = extension[k];
+	}
+	this._insted = true;
 }
-YANCL.prototype.postproc = [];
-YANCL.prototype.preproc = [];
-YANCL.prototype.procadd = function(type, proc)
+// context
+Yancl.prototype._insted = false;
+
+// processors
+Yancl.prototype._postproc = [];
+Yancl.prototype._preproc = [];
+Yancl.prototype._procadd = function(type, proc)
 {
 	if (typeof proc == 'function')
 	{
 		this[type + 'proc'].push(proc);
 	}
 }
-YANCL.prototype.process = function(type, data)
+Yancl.prototype._process = function(type, data)
 {
 	var processors = this[type + 'proc'];
 	for (p in processors)
@@ -37,8 +52,43 @@ YANCL.prototype.process = function(type, data)
 	}
 	return data;
 }
-YANCL.prototype.go = function(reqOpts)
-{/*
+
+//// create instance
+//function _spawn(extension)
+//{
+//	//for (k in this)
+//	//{
+//	//	if (typeof this[k] != 'function' && typeof extension[k] != 'undefined')
+//	//	{
+//	//		extension[k] = this[k];
+//	//	}
+//	//}
+//	return new Yancl(extension);
+//}
+
+// configure by methods
+function _method(m)
+{
+	return function(url)
+	{
+		return new Yancl({
+			method: m,
+			url: url
+		});
+	};
+}
+Yancl.get = _method('GET');
+Yancl.post = _method('POST');
+Yancl.put = _method('PUT');
+Yancl.patch = _method('PATCH');
+Yancl.delete = _method('DELETE');
+Yancl.head = _method('HEAD');
+Yancl.options = _method('OPTIONS');
+
+module.exports = Yancl;
+
+/*Yancl.prototype.send = function(reqOpts)
+{
 	http.get('http://m.countdown.tfl.gov.uk/arrivals/48704', function(res)
 	{
 		res.setEncoding('utf8');
@@ -58,5 +108,5 @@ YANCL.prototype.go = function(reqOpts)
 			var d = c.attr('src');
 			console.log(d);
 		});
-	});*/
-}
+	});
+}*/
